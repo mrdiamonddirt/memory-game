33aa4fd051c1c7398c7ac01e69718131033844ad
RowDog <mrdiamonddirt@gmail.com>
2022-10-27 16:29:12 +0100

    add functionality to remove cover from tile

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   //tiles
- const cards = ["","","","","","","","","","","","","","","",""]
+  const cards = [
+    "A1",
+    "A2",
+    "B1",
+    "B2",
+    "C1",
+    "C2",
+    "D1",
+    "D2",
+    "E1",
+    "E2",
+    "F1",
+    "F2",
+    "H1",
+    "H2",
+    "I1",
+    "I2",
+  ];
 
   return (
     <div className="App">
-      <div className='displaydiv'>
-    <Cards array={cards}/>
-    </div>
+      <div className="displaydiv">
+        {cards.map((card ,index) => {
+          return <Cards value={card} key={index}/>;
+        })}
+      </div>
     </div>
   );
 }
 
 function Cards(props) {
-  return(
-<>
-{props.array.map(() => {
-  return(
-    <div className='cards'><div className='holder'>A</div></div>
-  )
+  const [style, setStyle] = useState("style");
 
-})}
-</>
-  )}
+  const handleClick = () => {
+    console.log({ props });
+    setStyle("style2");
+  };
+
+  return (
+    <>
+      <div className={"card"} onClick={handleClick}>
+        <div className={style}>A</div>
+      </div>
+    </>
+  );
+}
 
 export default App;
