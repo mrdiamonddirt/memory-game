6bf324f7545666dd7cac81b758a6bb90012c6ce1
RowDog <mrdiamonddirt@gmail.com>
2022-10-27 12:37:45 +0100

    file setup creating 16 tiles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //tiles
+ const cards = ["","","","","","","","","","","","","","","",""]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='displaydiv'>
+    <Cards array={cards}/>
+    </div>
     </div>
   );
 }
+
+function Cards(props) {
+  return(
+<>
+{props.array.map(() => {
+  return(
+    <div  className='cards' >A</div>
+  )
+
+})}
+</>
+  )}
 
 export default App;
